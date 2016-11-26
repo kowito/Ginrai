e890cd086c5db8ad67bf95b81eb0368543e33346
Kowit Charoenratchatabhan <kowit@cookly.me>
2016-11-27 05:19:32 +0700

    FCrash Reporting

--- app/src/main/java/me/cookly/ginrai/EmailPasswordActivity.java
+++ app/src/main/java/me/cookly/ginrai/EmailPasswordActivity.java
@@ -9,12 +9,30 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import 	android.util.Base64;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.crash.FirebaseCrash;
 
 public class EmailPasswordActivity extends BaseActivity implements
         View.OnClickListener {
@@ -27,12 +45,32 @@ public class EmailPasswordActivity extends BaseActivity implements
     private EditText mPasswordField;
 
     private FirebaseAuth mAuth;
-
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private CallbackManager mCallbackManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "me.cookly.ginrai",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (NameNotFoundException e) {
+            FirebaseCrash.log("NameNotFoundException");
+
+
+
+        } catch (NoSuchAlgorithmException e) {
+            FirebaseCrash.log("NoSuchAlgorithmException");
+
+
+        }
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_email_password);
 
         mStatusTextView = (TextView) findViewById(R.id.status);
@@ -59,7 +97,33 @@ public class EmailPasswordActivity extends BaseActivity implements
                 updateUI(user);
             }
         };
+
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.facebook_sign_in_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
     }
+
+
 
     @Override
     public void onStart() {
@@ -131,7 +195,27 @@ public class EmailPasswordActivity extends BaseActivity implements
         mAuth.signOut();
         updateUI(null);
     }
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+        showProgressDialog();
 
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        hideProgressDialog();
+                    }
+                });
+    }
     private boolean validateForm() {
         boolean valid = true;
 
@@ -153,6 +237,15 @@ public class EmailPasswordActivity extends BaseActivity implements
 
         return valid;
     }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 
     private void updateUI(FirebaseUser user) {
         hideProgressDialog();
