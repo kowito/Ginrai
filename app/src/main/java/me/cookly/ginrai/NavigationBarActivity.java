57637e97e543ae90790297a1bf83b464f7e2a73d
Watt Iamsuri <wattiamsuri@Watts-MacBook-Pro.local>
2016-11-26 13:56:29 +0700

    add fragments

--- app/src/main/java/me/cookly/ginrai/NavigationBarActivity.java
+++ app/src/main/java/me/cookly/ginrai/NavigationBarActivity.java
@@ -1,5 +1,6 @@
 package me.cookly.ginrai;
 
+import android.app.FragmentTransaction;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,10 @@ import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
 public class NavigationBarActivity extends AppCompatActivity {
+
+
+    private ShareFragment shareFragment = null;
+    private LiveFeedFragment liveFeedFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +28,23 @@ public class NavigationBarActivity extends AppCompatActivity {
                     // The tab with id R.id.tab_favorites was selected,
                     // change your content accordingly.
                     System.out.println("share");
+                    if(shareFragment == null) {
+                        shareFragment = new ShareFragment();
+                        System.out.println("New Fragment");
+                    }
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    ft.replace(R.id.homepageContentContainer, shareFragment).commit();
                 }
                 if (tabId == R.id.tab_live_feed) {
                     // The tab with id R.id.tab_favorites was selected,
                     // change your content accordingly.
                     System.out.println("live feed");
+                    if(liveFeedFragment == null) {
+                        liveFeedFragment = new LiveFeedFragment();
+                        System.out.println("New Fragment");
+                    }
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    ft.replace(R.id.homepageContentContainer, liveFeedFragment).commit();
                 }
             }
         });
