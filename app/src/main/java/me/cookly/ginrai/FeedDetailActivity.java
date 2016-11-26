fee76a2af00e08028f71a23f4978687c70f54ea0
Watt Iamsuri <wattiamsuri@Watts-MacBook-Pro.local>
2016-11-26 23:07:22 +0700

    place picker

--- app/src/main/java/me/cookly/ginrai/FeedDetailActivity.java
+++ app/src/main/java/me/cookly/ginrai/FeedDetailActivity.java
@@ -1,15 +1,25 @@
 package me.cookly.ginrai;
 
+import android.content.Intent;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 import java.util.HashMap;
 
 public class FeedDetailActivity extends AppCompatActivity {
 
     public static final String extra = "lksd32iuo";
+
+    private int PLACE_PICKER_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +29,30 @@ public class FeedDetailActivity extends AppCompatActivity {
         HashMap<String, Object> hash = (HashMap<String, Object>) getIntent().getSerializableExtra(extra);
         TextView title = (TextView) findViewById(R.id.textViewFeedTitle);
         title.setText(hash.get("dish").toString());
+
+        Button back = (Button) findViewById(R.id.buttonFeedActivityBack);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+                try {
+                    startActivityForResult(builder.build(FeedDetailActivity.this), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                String toastMsg = String.format("Place: %s", place.getName());
+                System.out.println(toastMsg);
+            }
+        }
     }
 }
