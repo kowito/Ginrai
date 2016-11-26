56bb892f22486f6532437a6cc3df8ac554fa66a9
Watt Iamsuri <wattiamsuri@Watts-MacBook-Pro.local>
2016-11-26 13:48:12 +0700

    use bottom bar, add icons

--- app/src/main/java/me/cookly/ginrai/NavigationBarActivity.java
+++ app/src/main/java/me/cookly/ginrai/NavigationBarActivity.java
@@ -1,7 +1,11 @@
 package me.cookly.ginrai;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class NavigationBarActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class NavigationBarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_bar);
+
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_share) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+                    System.out.println("share");
+                }
+                if (tabId == R.id.tab_live_feed) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+                    System.out.println("live feed");
+                }
+            }
+        });
     }
 }
